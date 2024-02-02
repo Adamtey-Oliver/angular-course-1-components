af57f051fdcfcb4809e2af81a504cbe712220ef5
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2024-02-02 10:47:13 +0000

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
+  // standalone:  true,
+  // imports: [CourseCardComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
